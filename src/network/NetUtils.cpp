7f747f9c95c4d1dca05d9458b0f6c530c64384b9
Joao <j.a.c.buschinelli@gmail.com>
2024-04-12 11:12:04 -0300

    implemented routetableipv4 member functions

--- src/network/NetUtils.cpp
+++ src/network/NetUtils.cpp
@@ -128,3 +128,77 @@ struct in6_addr parasyte::network::utils::StringToAddress(std::string address) {
   inet_pton(AF_INET6, address.c_str(), &(addr));
   return addr;
 }
+
+/* Classes implementation */
+
+/**
+ * Constructor for the RouteTableIPv4 class.
+ * Reads the route information from the /proc/net/route file and initializes the route_info_list_ member variable.
+ */
+parasyte::network::utils::RouteTableIPv4::RouteTableIPv4() {
+  std::ifstream route_table_ipv4_proc(proc_route_ipv4_);
+  InitStream(route_table_ipv4_proc);
+  for (RouteInfoIPv4 route_info_ipv4; ReadRouteInfo(route_table_ipv4_proc, route_info_ipv4);) {
+    route_info_list_.push_back(route_info_ipv4);
+  }
+}
+
+/**
+ * Finds the default IPv4 route in the route table.
+ * 
+ * @return An iterator pointing to the default IPv4 route, or `route_info_list_.end()` if not found.
+ */
+std::vector<parasyte::network::utils::RouteInfoIPv4>::const_iterator parasyte::network::utils::RouteTableIPv4::DefaultIPv4Route() const {
+  // Use std::find_if algorithm to search for the default IPv4 route in the route_info_list_
+  return std::find_if(route_info_list_.begin(), route_info_list_.end(), [](RouteInfoIPv4 const& route_info){
+    // The lambda function is the callback that is passed to std::find_if
+    // It takes a RouteInfoIPv4 object as input and returns a boolean value
+    // The lambda function checks if the destination address of the route_info object is equal to boost::asio::ip::address_v4()
+    // If it is, it returns true and std::find_if stops the search and returns the iterator pointing to the current route_info object
+    // If it is not, it returns false and std::find_if continues the search
+    return route_info.dest == boost::asio::ip::address_v4();
+  });
+}
+
+/**
+ * Finds the route in the route table that matches the given target IPv4 address.
+ * 
+ * @param target The target IPv4 address to find in the route table.
+ * @return An iterator pointing to the matching route, or `route_info_list_.end()` if not found.
+ */
+std::vector<parasyte::network::utils::RouteInfoIPv4>::const_iterator parasyte::network::utils::RouteTableIPv4::Find(boost::asio::ip::address_v4 target) const {
+  std::vector<RouteInfoIPv4>::const_iterator default_route_table = DefaultIPv4Route(); // Get the iterator pointing to the default IPv4 route in the route table
+  std::vector<RouteInfoIPv4>::const_iterator it = route_info_list_.begin(); // Initialize the iterator to the beginning of the route_info_list_
+
+  for (; it != route_info_list_.end(); ++it) { // Iterate through each route in the route_info_list_
+    if (it == default_route_table) continue; // Skip the default route
+    if (boost::asio::ip::address_v4::broadcast(target, it -> netmask) == it -> dest) break; // Check if the broadcast address of the target matches the destination address of the current route
+  }
+
+  return (it == route_info_list_.end()) ? default_route_table : it; // Return the iterator pointing to the matching route, or the iterator pointing to the default route if no match is found
+}
+
+/**
+ * @brief The std::istream class is a base class for input streams.
+ * 
+ * It provides a common interface for reading data from different sources, such as files, strings, or network connections.
+ * InitStream is a virtual function that initializes the input stream by reading a line of text from the stream.
+ * @param stream The input stream to initialize.
+ * @return The initialized input stream.
+ * @see https://en.cppreference.com/w/cpp/io/basic_istream
+ */
+std::istream &parasyte::network::utils::RouteTableIPv4::InitStream(std::istream &stream) {
+  std::string line;
+  return std::getline(stream, line);
+}
+
+std::ifstream &parasyte::network::utils::RouteTableIPv4::ReadRouteInfo(std::ifstream &stream, RouteInfoIPv4 &route_info) {
+  uint32_t dest, gateway, netmask;
+
+  stream >> route_info.name >> std::hex >> dest >> gateway >> std::dec >> route_info.flags >> route_info.ref_count >> route_info.use >> route_info.metric >> std::hex >> netmask >> std::dec >> route_info.mtu >> route_info.window >> route_info.ip_route_table;
+  route_info.dest = boost::asio::ip::address_v4(ntohl(dest));
+  route_info.gateway = boost::asio::ip::address_v4(ntohl(gateway));
+  route_info.netmask = boost::asio::ip::address_v4(ntohl(netmask));
+
+  return stream;
+}
